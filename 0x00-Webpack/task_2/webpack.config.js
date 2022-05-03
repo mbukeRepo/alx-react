535c98f79384a36d4327af3eeb79017975c61fc2
mbukeRepo <mbuke404@gmail.com>
2022-05-03 20:02:40 +0200

    small tweak

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -17,11 +17,33 @@ module.exports =  {
         use: ['style-loader', 'css-loader']
       },
       {
-        test:  /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        use: [{loader: 'image-webpack-loader', options: {
-          bypassOnDebug: true,
-          disable: true
-        }}]
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+             loader: 'image-webpack-loader',
+             options: {
+               mozjpeg: {
+               progressive: true,
+             },
+             // optipng.enabled: false will disable optipng
+             optipng: {
+               enabled: false,
+             },
+             pngquant: {
+               quality: [0.65, 0.90],
+               speed: 4
+             },
+             gifsicle: {
+               interlaced: false,
+             },
+             // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
+        ],
       }
     ]
   }
