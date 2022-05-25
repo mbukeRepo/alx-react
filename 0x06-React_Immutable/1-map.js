750d933b4a68a6968877af65b991b03c3e0c42eb
mbukeRepo <mbuke404@gmail.com>
2022-05-25 21:50:02 +0200

    Converting into Immutable using Map

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -1,5 +1,5 @@
-const { Map } = require('immutable');
-const getImmutableObject = (object) => {
-  return Map(object);
-}
+import { Map } from "immutable";
+
+const getImmutableObject = (object) => Map(object);
+
 export default getImmutableObject;
