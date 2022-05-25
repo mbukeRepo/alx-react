4fc8b69cdfa4b2ce09b5cfd031ec437a1209bec0
mbukeRepo <mbuke404@gmail.com>
2022-05-25 21:34:24 +0200

    little adjustment

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,9 +1,6 @@
 const Immutable = require('immutable');
-const obj = {
-     fear: true,
-     smell: -1033575916.9145899,
-     wall: false,
-     thing: -914767132
-};
-const map = Immutable.fromJS(obj);
-console.log(map);
+
+function getImmutableObject(object) {
+ return Immutable.fromJS(object);
+}
+
