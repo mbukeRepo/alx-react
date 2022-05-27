f7601c2fc0ec0839c5588879845487705ae7f0ca
mbukeRepo <mbuke404@gmail.com>
2022-05-27 19:42:07 +0200

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,16 +2,27 @@ import { Seq } from 'immutable';
 
 const printBestStudents = (object) => {
   const seq = Seq(object);
-  const getStudent =  seq.filter(value => value.score < 70);
+  const getStudent =  seq.filter(value => value.score > 70);
   const student = getStudent.toJS();
   const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
-  Object.keys(student).map((key) => {
+  Object.keys(student).forEach((key) => {
     student[key].firstName = formatName(student[key].firstName);
     student[key].lastName = formatName(student[key].lastName);
-    return student[key];
   });
 
   console.log(student);
 };
 
+// testing
+/**
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  }
+};
+
+printBestStudents(grades);
+*/
 export default printBestStudents;
