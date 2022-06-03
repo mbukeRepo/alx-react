7400fc6a5ed866b481719714d1757d7128ceb2dd
mbukeRepo <mbuke404@gmail.com>
2022-06-03 11:22:28 +0200

    Async Action Creators

--- 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.test.js
@@ -1,5 +1,5 @@
-import {LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER} from './uiActionTypes.js';
-import {login, logout, displayNotificationDrawer, hideNotificationDrawer} from './uiActionCreators.js';
+import {LOGIN, LOGIN_FAILURE, LOGIN_SUCCESS, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER} from './uiActionTypes.js';
+import {login, logout, displayNotificationDrawer, hideNotificationDrawer, loginRequest} from './uiActionCreators.js';
 
 describe('tests for action creators', () => {
   it('login action creator', () => {
@@ -19,5 +19,9 @@ describe('tests for action creators', () => {
   it('hideNotificationDrawer', () => {
     const action = {type: HIDE_NOTIFICATION_DRAWER};
     expect(hideNotificationDrawer()).toEqual(action);
+  });
+  it('testing loginRequest if request is successful', () => {
+    const actions = [{type: LOGIN, user: {email: 'test@test.com', password: '123'}}, {type: LOGIN_SUCCESS}]
+    expect(loginRequest('test@test.com', '123')).toEqual(actions);
   });
 });
