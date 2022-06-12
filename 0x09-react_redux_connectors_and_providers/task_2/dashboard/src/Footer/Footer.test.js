5959052132e0a337b17efd4e946e663d98fc6a67
mbukeRepo <mbuke404@gmail.com>
2022-06-12 23:37:36 +0200

    Modify the test suites

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.test.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Footer from './Footer';
+import { shallow, mount } from "enzyme";
+import React from "react";
+import { Footer } from "./Footer";
+import AppContext from "../App/AppContext";
+import { user, logOut } from "../App/AppContext";
+import { shallowEqual } from "react-redux";
 
-describe('<Footer />', () => {
-  const wrapper = shallow(<Footer />);
-
-  it('render without crashing', () => {
-    expect(wrapper.exists());
+describe("<Footer />", () => {
+  it("Footer renders without crashing", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("Verify that the components at the very least render the text “Copyright”", () => {
+    const wrapper = shallow(<Footer />);
+    expect(wrapper.find("div.footer p")).toHaveLength(1);
+    expect(wrapper.find("div.footer p").text()).toContain("Copyright");
   });
 
-//  it('paragraph and content', () => {
-//    expect(wrapper.find('footer p')).toHaveLength(1);
-//    expect(wrapper.find('footer p').text()).toContain('Copyright');
-//  });
+  it("verify that the link is not displayed when the user is logged out within the context", () => {
+    const wrapper = shallow(<Footer user={null} />);
+    expect(wrapper.find("div.footer a")).toHaveLength(0);
+  });
+
+  it("verify that the link is displayed when the user is logged in within the context", () => {
+    const wrapper = shallow(
+      <Footer user={{ email: "larry@hd.com", password: "123456" }} />
+    );
+    expect(wrapper.find("div.footer a")).toHaveLength(1);
+    expect(wrapper.find("div.footer a").text()).toEqual("Contact us");
+  });
 });
