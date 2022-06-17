1dff8f9eded1d420c0f581313627be9816434530
mbukeRepo <mbuke404@gmail.com>
2022-06-17 23:14:33 +0200

    Container/Component

---
+++ 0x09-react_redux_connectors_and_providers/task_9/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+
+class NotificationItem extends React.PureComponent {
+  onClickHandler =  () =>  {
+    this.props.markAsRead(this.props.guid);
+  }
+ 
+  render() {
+    const { type, html, value } = this.props;
+    const color = css(type === 'urgent' ? styles.urgent : styles.default);
+    
+    let li;
+    value
+      ? (li = (
+          <li
+            className={color}
+            data-notification-type={type}
+            onClick={this.onClickHandler}
+          >
+            {value}
+          </li>
+        ))
+      : (li = (
+          <li
+            className={color}
+            data-notification-type={type}
+            dangerouslySetInnerHTML={html}
+            onClick={this.onClickHandler}
+          ></li>
+        ));
+
+    return li;
+  }
+}
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  html: {},
+  value: '',
+  markAsRead: () => {},
+  id: NaN,
+};
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string,
+  }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func,
+  id: PropTypes.number,
+};
+
+const styles = StyleSheet.create({
+  default: {
+    color: 'blue',
+  },
+
+  urgent: {
+    color: 'red',
+  },
+});
+
+export default NotificationItem;
